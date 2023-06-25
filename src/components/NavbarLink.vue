fce6c19f9447bb72f79c9c2eddac7a500de39535
azer <azer@azers-MacBook-Pro.local>
2023-06-25 14:55:34 +0400

    vue cli,folder structure,navbar,bootstrap,v-for,props,created methods,style

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,31 @@
+<template>
+  <a
+    :href="page.link.url"
+    :title="`This link goes to the \${page.link.text} page`"
+    aria-current="page"
+    class="nav-link"
+    :class="activeClasses"
+    >{{ page.link.text }}</a
+  >
+</template>
+
+<script>
+export default {
+    props:['page','isActive'],
+    computed:{
+        activeClasses(){
+            return {
+                active:this.isActive,
+                emphasize:this.isActive
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+.emphasize{
+    text-decoration: underline !important;
+}
+</style>
